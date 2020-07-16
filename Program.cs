3fcbd3f595390432b40dd65e14fde2ff5cf4627a
Iain Black <iain.black@moodys.com>
2020-07-16 15:24:59 +0100

    Simple app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Package_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
